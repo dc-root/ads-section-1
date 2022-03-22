d361510bc2b96fac02b1cf9c8ec4ceceb294efeb
Diego Silva <diegocaetano444@outlook.com>
2022-03-22 07:59:13 -0300

    Prog. ii: uploand question seven in list five

--- ads-section-2/programacao-de-computadores-ii/lista-de-exercicios-revisao-05/exemplo_questao-07.c
+++ ads-section-2/programacao-de-computadores-ii/lista-de-exercicios-revisao-05/exemplo_questao-07.c
@@ -1,49 +1,174 @@
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <ctype.h>
+
+// #define QUANTIDADE_DE_SERVICOS 7
+
+// typedef struct {
+//   int dia;
+//   int mes;
+//   int ano
+// } tData;
+
+// typedef struct {
+//   char nome[200];
+// } tServicos;
+
+// typedef struct {
+//   int numeroOrdemCervico;
+//   tData dataCervico;
+//   float valor;
+//   tServicos Servico[QUANTIDADE_DE_SERVICOS];
+//   char nomeCliente[50];
+// } tOrdemServico, *pOrdemServico;
+
+// void main() {
+//   tOrdemServico OS;
+//   pOrdemServico pOS = &OS;
+
+//   const tOrdemServico Servicos = {
+//     .Servico = {
+//       { .nome = "Manutenção da embreagem" },
+//       { .nome = "Troca de óleo do motor" },
+//       { .nome = "Troca de óleo do câmbio automático" },
+//       { .nome = "Troca de filtros" },
+//       { .nome = "Troca de lâmpadas" },
+//       { .nome = "Alinhamento e Balanceamento" },
+//       { .nome = "Revisão no Sistema de Arrefecimento" }
+//     }
+//   };
+  
+//   for (int i=0; i<7; i++) {
+//     printf("\n%s", Servicos.Servico[i].nome);
+
+//   }
+  
+//   // Lendo as informações do usuário
+  
+//   getchar();
+// }
+
+
+// ############################################################################
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
+#include <time.h>
+#include <string.h>
+#include <unistd.h>
 
-#define QUANTIDADE_DE_SERVICOS 7
+#define TIPOS_DE_SERVICOS 8
+#define SOLICITACAO_DE_SERVICO 3 // 100
 
-typedef struct {
-  int dia;
-  int mes;
-  int ano
-} tData;
+// Função par limpar o buffer
+void clearBuffer(void) {
+  char c;
+  do {
+    c = getchar();
+  } while ((c!='\n') && (c!=EOF));
+}
 
+// Estrutura do serviço solicitado pelo cliente
 typedef struct {
   char nome[200];
-} tServicos;
+  char descricao[500];
+} tServicos, *pServicos;
 
+// Estrutura das ordems de serviço
 typedef struct {
-  int numeroOrdemCervico;
-  tData dataCervico;
+  int numeroOrdemServico;
+  char dataServico[100];
   float valor;
-  tServicos Servico[QUANTIDADE_DE_SERVICOS];
+  tServicos servicoPrestado[TIPOS_DE_SERVICOS];
   char nomeCliente[50];
 } tOrdemServico, *pOrdemServico;
 
 void main() {
   tOrdemServico OS;
   pOrdemServico pOS = &OS;
+  int sair=1, option=0, qntOS=0;
+  float mediaValores=0, somaValores=0, servicoMaisCaro=0;
+  char nomeClienteMaisCaro[50], nomeServicoMaisCaro[200], descrClienteMaisCaro[500], dataClienteMaisCaro[100];
 
-  const tOrdemServico Servicos = {
-    .Servico = {
-      { .nome = "Manutenção da embreagem" },
-      { .nome = "Troca de óleo do motor" },
-      { .nome = "Troca de óleo do câmbio automático" },
-      { .nome = "Troca de filtros" },
-      { .nome = "Troca de lâmpadas" },
-      { .nome = "Alinhamento e Balanceamento" },
-      { .nome = "Revisão no Sistema de Arrefecimento" }
-    }
+  time_t now;
+
+  tServicos servicos[TIPOS_DE_SERVICOS];
+  pServicos pS = &pOS->servicoPrestado[0];
+
+  // Serviços que a oficina oferece
+  char string[TIPOS_DE_SERVICOS][200] = {
+    "Manutenção da embreagem",
+    "Revisão dos componentes de Freio",
+    "Revisão no Sistema de Arrefecimento",
+    "Troca de óleo do motor",
+    "Troca de óleo do câmbio automático",
+    "Troca de filtros",
+    "Troca de lâmpadas",
+    "Alinhamento e Balanceamento"
   };
-  
-  for (int i=0; i<7; i++) {
-    printf("\n%s", Servicos.Servico[i].nome);
 
+  // vinculando-os a estrutura de ordem de serviço
+  for (int i=0; i<TIPOS_DE_SERVICOS; i++) {
+    strcpy(pS->nome, string[i]);
+    pS++;
   }
-  
-  // Lendo as informações do usuário
-  
+
+  // Lendo as informações do usuário da O.S
+  for (int i=0; i<SOLICITACAO_DE_SERVICO; i++, pOS++) {
+    printf("\n\t1. Continuar(Solicitar serviço)\n\t0. Cancelar\n\nDigte uma das opções acima: ");
+    scanf("%d", &sair);
+
+    if (sair == 0) break;
+
+    // Data do serviço
+    pOS->numeroOrdemServico = 10000 + rand() % 99999;
+    printf("\nNumero da Ordem de Servico: %d\n", pOS->numeroOrdemServico);
+    
+    now = time(NULL);
+    strcpy(pOS->dataServico, ctime(&now));
+    printf("Data da ocorrencia do serviço: %s\n", pOS->dataServico);
+
+    // valor do serviço
+    printf("Digite o valor do serviço prestado(R$): ");
+    scanf("%f", &pOS->valor);
+
+    // Serviço prestado
+    for (int i=0; i<TIPOS_DE_SERVICOS; i++) {
+      printf("\n%d. %s", i+1, pOS->servicoPrestado[i].nome);
+    }
+
+    clearBuffer();
+    printf("\n\nQual serviço foi prestado: ");
+    scanf("%d", &option);
+
+    // Descrição do serviço
+    printf("\nDescreva o que foi feito\n> ");
+    fgets(pOS->servicoPrestado[option].descricao, 500, stdin);
+
+    // Nome do cliente
+    clearBuffer();
+    printf("\nNome do cliente: ");
+    fgets(pOS->nomeCliente, 50, stdin);
+
+    qntOS++;
+    somaValores += pOS->valor;
+
+    if (pOS->valor > servicoMaisCaro) {
+      strcpy(nomeClienteMaisCaro, pOS->nomeCliente);
+      strcpy(nomeServicoMaisCaro, pOS->servicoPrestado->nome);
+      strcpy(descrClienteMaisCaro, pOS->servicoPrestado->descricao);
+      strcpy(dataClienteMaisCaro, pOS->dataServico);
+    }
+  }
+
+  mediaValores = somaValores / qntOS;
+  printf("Media dos valores: R$%.2f\n", mediaValores);
+
+  printf("\n\nCLIENTE MAIS CARO\n\n");
+  printf("nome: %s\n", nomeClienteMaisCaro);
+  printf("serviço prestado: %s\n", nomeServicoMaisCaro);
+  printf("descrição do serviço: %s\n", descrClienteMaisCaro);
+  printf("data de realização: %s\n", dataClienteMaisCaro);
+
   getchar();
 }
--- ads-section-2/programacao-de-computadores-ii/lista-de-exercicios-revisao-05/questao-07.c
+++ ads-section-2/programacao-de-computadores-ii/lista-de-exercicios-revisao-05/questao-07.c
@@ -4,117 +4,138 @@
 #include <string.h>
 #include <unistd.h>
 
-#define TIPOS_DE_SERVICOS 8
-#define SOLICITACAO_DE_SERVICO 3 // 100
+#define NUM_SERVICOS 100 // Quantidade de serviços que a oficina mecânica pode realizar
+#define NUM_OS 100 // Limite de serviços que podem ser ordenados
 
-// Função par limpar o buffer
-void clearBuffer(void) {
-  char c;
-  do {
-    c = getchar();
-  } while ((c!='\n') && (c!=EOF));
-}
-
-// Estrutura do serviço solicitado pelo cliente
+// DEFININDO AS ESTRUTURAS
 typedef struct {
-  char nome[200];
-  char descricao[500];
-} tServicos, *pServicos;
+  char nomeServico[100];
+  char descricaoServico[500];
+} tServicos;
 
-// Estrutura das ordems de serviço
 typedef struct {
   int numeroOrdemServico;
-  char dataServico[100];
-  float valor;
-  tServicos servicoPrestado[TIPOS_DE_SERVICOS];
+  char dataEmissaoOrdemServico[100];
+  tServicos servicoEmitido;
+  float valorServico;
   char nomeCliente[50];
 } tOrdemServico, *pOrdemServico;
 
+tServicos vServicos[NUM_SERVICOS];
+tOrdemServico OS[NUM_OS];
+pOrdemServico pOS = OS;
+
+int countG=0;
+
+// DECLARANDO AS FUNÇÕES
+void cadastrarServico(char *nome, char *description);
+void registrarOrdemServico(float *soma_vs, char *nome_cliente_smc, float *valor_smc, char *data_smc, char *descricao_smc);
+
 void main() {
-  tOrdemServico OS;
-  pOrdemServico pOS = &OS;
-  int sair=1, option=0, qntOS=0;
-  float mediaValores=0, somaValores=0, servicoMaisCaro=0;
-  char nomeClienteMaisCaro[50], nomeServicoMaisCaro[200], descrClienteMaisCaro[500], dataClienteMaisCaro[100];
+  int optionMenu=0, countServico=0, countOrdemServico=0;
+  float somaValoresServicos=0.0, mediaValoreServico=0, valorServicoMaisCaro=0.0;
+  char nomeClienteMaisCaro[50], dataEmissaoServicoMaisCaro[100], descricaoClienteMaisCaro[500];
 
+  while (countServico < NUM_SERVICOS) {
+    // Menu
+    printf("\n\t1. Cadastrar novo serviços\n\t2. Sair\n\nEscolha uma das opções acima: ");
+    scanf("%i%*c", &optionMenu);
+
+    if (optionMenu == 1) {
+      cadastrarServico(vServicos[countServico].nomeServico, vServicos[countServico].descricaoServico);
+
+      countServico++;
+    } else if (optionMenu == 2)
+      break;
+    else;
+  }
+
+  countG=countServico;
+
+  // sleep(2);
+  // printf("\nOK, Serviços cadastrado com sucesso!!\n");
+  // sleep(2);
+
+  // Lenitura das OS
+  while (countOrdemServico < NUM_OS) {
+    // Menu
+    printf("\n\t1. Registrar Ordem de Serviço\n\t2. Sair\n\nEscolha uma das opções acima: ");
+    scanf("%i%*c", &optionMenu);
+    if (optionMenu == 1) {
+      registrarOrdemServico(&somaValoresServicos, nomeClienteMaisCaro, &valorServicoMaisCaro, dataEmissaoServicoMaisCaro, descricaoClienteMaisCaro);
+
+      countOrdemServico++;
+      pOS++;
+    } else if (optionMenu == 2)
+      break;
+    else;
+  }
+
+  // Mostrando Resultado
+  printf("\n\n################################ INFORMATION ################################\n\n");
+  
+  printf("Media dos valores entre os serviços ordenados registrados: R$%.2f\n\n", mediaValoreServico);
+  mediaValoreServico = somaValoresServicos / countOrdemServico;
+
+  printf("Cliente com maior oferta: \t%s\n", nomeClienteMaisCaro);
+  printf("Valor do serviço: \tR$%.2f\n", valorServicoMaisCaro);
+  printf("Descrição: \t%s\n", descricaoClienteMaisCaro);
+  printf("Data da solicitação: \t%s", dataEmissaoServicoMaisCaro);
+
+  printf("\n\n##############################################################################\n\n");
+}
+
+// DEFININDO AS FUNÇÕES
+
+// Função para cadastro de serviços
+void cadastrarServico(char *nome, char *description) {
+  // Nome do serviço
+  printf("\nDigite o nome do serviço: ");
+  fgets(nome, 100, stdin);
+
+  // Descrição do serviço
+  printf("Descrição:\n> ");
+  fgets(description, 500, stdin);
+}
+
+void registrarOrdemServico(float *soma_vs, char *nome_cliente_smc, float *valor_smc, char *data_smc, char *descricao_smc) {
+  int opcaoServico;
+
+  // Numero da OS
+  pOS->numeroOrdemServico = 10000000 + rand() % 99999999;
+  printf("\nNumero de OS: %i", pOS->numeroOrdemServico);
+
+  // Data da OS
   time_t now;
+  now = time(NULL);
 
-  tServicos servicos[TIPOS_DE_SERVICOS];
-  pServicos pS = &pOS->servicoPrestado[0];
+  strcpy(pOS->dataEmissaoOrdemServico, ctime(&now));
+  printf("\n%s\n", pOS->dataEmissaoOrdemServico);
 
-  // Serviços que a oficina oferece
-  char string[TIPOS_DE_SERVICOS][200] = {
-    "Manutenção da embreagem",
-    "Revisão dos componentes de Freio",
-    "Revisão no Sistema de Arrefecimento",
-    "Troca de óleo do motor",
-    "Troca de óleo do câmbio automático",
-    "Troca de filtros",
-    "Troca de lâmpadas",
-    "Alinhamento e Balanceamento"
-  };
-
-  // vinculando-os a estrutura de ordem de serviço
-  for (int i=0; i<TIPOS_DE_SERVICOS; i++) {
-    strcpy(pS->nome, string[i]);
-    pS++;
+  // Leitura do serviço
+  for (int i=0; i<countG; i++) {
+    printf("%i. %s", i+1, vServicos[i].nomeServico);
   }
+  printf("\nEscolha uma das opções de serviço acima: ");
+  scanf("%i", &opcaoServico);
+  strcpy(pOS->servicoEmitido.nomeServico, vServicos[opcaoServico].nomeServico);
 
-  // Lendo as informações do usuário da O.S
-  for (int i=0; i<SOLICITACAO_DE_SERVICO; i++, pOS++) {
-    printf("\n\t1. Continuar(Solicitar serviço)\n\t0. Cancelar\n\nDigte uma das opções acima: ");
-    scanf("%d", &sair);
+  // Leitura do valor do serviço
+  printf("Digite o valor do serviço(R$): ");
+  scanf("%f%*c", &pOS->valorServico);
 
-    if (sair == 0) break;
+  // Leitura do nome do cliente
+  printf("Qual o nome do cliente: ");
+  fgets(pOS->nomeCliente, 50, stdin);
 
-    // Data do serviço
-    pOS->numeroOrdemServico = 10000 + rand() % 99999;
-    printf("\nNumero da Ordem de Servico: %d\n", pOS->numeroOrdemServico);
-    now = time(NULL);
-    strcpy(pOS->dataServico, ctime(&now));
-    printf("Data da ocorrencia do serviço: %s\n", pOS->dataServico);
+  // Soma valores
+  *soma_vs += pOS->valorServico;
 
-    // valor do serviço
-    printf("Digite o valor do serviço prestado(R$): ");
-    scanf("%f", &pOS->valor);
-
-    // Serviço prestado
-    for (int i=0; i<TIPOS_DE_SERVICOS; i++) {
-      printf("\n%d. %s", i+1, pOS->servicoPrestado[i].nome);
-    }
-
-    clearBuffer();
-    printf("\n\nQual serviço foi prestado: ");
-    scanf("%d", &option);
-
-    // Descrição do serviço
-    printf("\nDescreva o que foi feito\n> ");
-    fgets(pOS->servicoPrestado[option].descricao, 500, stdin);
-
-    // Nome do cliente
-    clearBuffer();
-    printf("\nNome do cliente: ");
-    fgets(pOS->nomeCliente, 50, stdin);
-
-    qntOS++;
-    somaValores += pOS->valor;
-
-    if (pOS->valor > servicoMaisCaro) {
-      strcpy(nomeClienteMaisCaro, pOS->nomeCliente);
-      strcpy(nomeServicoMaisCaro, pOS->servicoPrestado->nome);
-      strcpy(descrClienteMaisCaro, pOS->servicoPrestado->descricao);
-      strcpy(dataClienteMaisCaro, pOS->dataServico);
-    }
+  // Cliente mais caro
+  if (*valor_smc < pOS->valorServico) {
+    strcpy(nome_cliente_smc, pOS->nomeCliente);
+    *valor_smc = pOS->valorServico;
+    strcpy(data_smc, pOS->dataEmissaoOrdemServico);
+    strcpy(descricao_smc, pOS->servicoEmitido.descricaoServico);
   }
-
-  mediaValores = somaValores / qntOS;
-  printf("Media dos valores: R$%.2f\n", mediaValores);
-
-  printf("\n\nCLIENTE MAIS CARO\n\n");
-  printf("nome: %s\n", nomeClienteMaisCaro);
-  printf("serviço prestado: %s\n", nomeServicoMaisCaro);
-  printf("descrição do serviço: %s\n", descrClienteMaisCaro);
-  printf("data de realização: %s\n", dataClienteMaisCaro);
-
-  getchar();
 }
