3c126bf38158c2b4c9196a35d7fdea4e59e7767a
Diego Silva <diegocaetano444@outlook.com>
2022-05-16 14:37:54 -0300

    Prog. ii: Added testes end update trabalho-01

--- ads-section-2/programacao-de-computadores-ii/trabalho-01-processamento-de-imagens/pgmmedianfilter.c
+++ ads-section-2/programacao-de-computadores-ii/trabalho-01-processamento-de-imagens/pgmmedianfilter.c
@@ -32,7 +32,6 @@ int main(int argc, char *argv[]) {
 
     // Verificação da mascara
     unsigned short flagMaskN = strtol(argv[1], NULL, 10);
-
     if(argv[2] && (flagMaskN > 0) && (flagMaskN % 2) > 0)
         mask = flagMaskN;
     else if (!argv[2]);
@@ -41,6 +40,8 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
+    unsigned short int matrizScanColorGrid[mask][mask];
+
     // Lendo cada elemento do cabeçalho do arquivo
     fscanf(pgmInput, "%s %hd %hd %hd",
             pontTypeStructPGM->type,
@@ -48,31 +49,30 @@ int main(int argc, char *argv[]) {
             &pontTypeStructPGM->line,
             &pontTypeStructPGM->colorVariance
     );
+
     // Lendo o corpo do arquivo (matriz)
     for(int i=0; i<pontTypeStructPGM->line; i++) {
         for(int j=0; j<pontTypeStructPGM->column; j++) {
             fscanf(pgmInput, "%hd", &matrizColorGrid[i][j]);
             printf("%hd  ", matrizColorGrid[i][j]);
-        }
+
+        } fscanf(pgmInput, "\n");
         printf("\n");
     }
 
     fclose(pgmInput);
-
+    
     // validações e formatações necessárias para o nome do arquivo de saida(fileNameOutput)
     char outputFileName[] = "output_img.pgm";
-   
-    // char prefix[] = "output_";
+    // char prefix[] = "output-";
     // char *fileName;
-    
     // fileName = strstr((argv[2] ? argv[2] : argv[1]), "/") != NULL ?
-    //                     strrchr((argv[2] ? argv[2] : argv[1]), '/') +1
+    //                     strrchr((argv[2] ? argv[2] : argv[1]), '/')+1
     //                     :
     //                     (argv[2] ? argv[2] : argv[1]);
-
     // strcat(prefix, fileName);
-    
     // size_t tm = strlen(prefix);
+    // printf("\nsize prefix: %d\n", tm);
     // char outputFileName[tm];
     // strcpy(outputFileName, prefix);
 
@@ -80,13 +80,12 @@ int main(int argc, char *argv[]) {
     FILE *pgmOutput;
     openEndVerifyFile(&pgmOutput, outputFileName, "w");
 
-    // Escrevendo cada elemento do cabeçalho do arquivo
+    // Escrevendo cada elemento do cabeçalho no arquivo
     fprintf(pgmInput, "%s\n%hd %hd\n%hd\n",
             pontTypeStructPGM->type,
             pontTypeStructPGM->column,
             pontTypeStructPGM->line,
-            pontTypeStructPGM->colorVariance
-    );
+            pontTypeStructPGM->colorVariance);
 
     // Escrevendo o corpo do arquivo (matriz)
     for(int i=0; i<pontTypeStructPGM->line; i++) {
@@ -119,7 +118,7 @@ void hError(char messageError[], _Bool man) {
 
     if(man) {
         // Manual
-        printf("\n\033[0;33mUsage:  pgmmedian  -m N -i input -o output\n\n\033[0m");
+        printf("\n\033[0;33mUsage:  pgmmedian  -m N -i input\n\n\033[0m");
         printf("\033[0;33mwithout mask:\tpgmmedian  [FILE-INPUT]\n\033[0m");
         printf("\033[0;33mwith mask:\tpgmmedian  [ODD-NUMBER-MASK] [FILE-INPUT]\n\033[0m");
     } else;
