674dce5a5a51cfb7a403bf81daff5b554f2236ef
Diego C. Silva <diegocaetano444@outlook.com>
2022-09-17 23:43:58 -0300

    PW: correction details

--- ads-section-3/programação-para-web/challenges/menu-bar/app.js
+++ ads-section-3/programação-para-web/challenges/menu-bar/app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const { Server } = require('http');
 const path = require('path');
 
 const app = express();
@@ -11,7 +10,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({extended: true}));
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.render("./index", { component: 'home-content' });
 });
 
